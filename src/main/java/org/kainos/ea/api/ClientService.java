3aa49d9373e3ceec0859923d68bfd3a40ed3c7cd
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2023-10-12 13:50:24 +0100

    Refactor client controller

--- src/main/java/org/kainos/ea/api/ClientService.java
+++ src/main/java/org/kainos/ea/api/ClientService.java
@@ -1,26 +1,47 @@
 package org.kainos.ea.api;
 
 import org.kainos.ea.cli.Client;
+import org.kainos.ea.cli.ClientOutput;
+import org.kainos.ea.cli.Project;
+import org.kainos.ea.cli.SalesEmployee;
 import org.kainos.ea.client.FailedToGetClientsException;
+import org.kainos.ea.client.FailedToGetProjectsException;
+import org.kainos.ea.client.FailedToGetSalesEmployeeException;
+import org.kainos.ea.client.SalesEmployeeDoesNotExistException;
 import org.kainos.ea.db.ClientDao;
+import org.kainos.ea.db.ProjectDao;
+import org.kainos.ea.db.SalesEmployeeDAO;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClientService {
     ClientDao clientDao = new ClientDao();
+    SalesEmployeeService salesEmployeeService = new SalesEmployeeService();
+    ProjectService projectService = new ProjectService();
 
-    public List<Client> getAllClients() throws FailedToGetClientsException {
-        List<Client> clientList;
+    public List<ClientOutput> getAllClientsInfo()
+            throws FailedToGetClientsException,
+            FailedToGetSalesEmployeeException,
+            SalesEmployeeDoesNotExistException,
+            FailedToGetProjectsException {
+        List<ClientOutput> clientOutputList = null;
 
         try {
-            clientList = clientDao.getAllClients();
+            clientOutputList = new ArrayList<>();
+            for (Client client: clientDao.getAllClients()) {
+                SalesEmployee salesEmployee = salesEmployeeService.getSalesEmployeeById(client.getSalesEmpId());
+                List<Project> projectList = projectService.getAllProjectsWithClient(client.getClientId());
+                ClientOutput clientOutput = new ClientOutput(client, salesEmployee, projectList);
+                clientOutputList.add(clientOutput);
+            }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
 
             throw new FailedToGetClientsException();
         }
 
-        return clientList;
+        return clientOutputList;
     }
 }
--- src/main/java/org/kainos/ea/resources/ClientController.java
+++ src/main/java/org/kainos/ea/resources/ClientController.java
@@ -2,12 +2,6 @@ package org.kainos.ea.resources;
 
 import io.swagger.annotations.Api;
 import org.kainos.ea.api.ClientService;
-import org.kainos.ea.api.ProjectService;
-import org.kainos.ea.api.SalesEmployeeService;
-import org.kainos.ea.cli.Client;
-import org.kainos.ea.cli.ClientOutput;
-import org.kainos.ea.cli.Project;
-import org.kainos.ea.cli.SalesEmployee;
 import org.kainos.ea.client.FailedToGetClientsException;
 import org.kainos.ea.client.FailedToGetProjectsException;
 import org.kainos.ea.client.FailedToGetSalesEmployeeException;
@@ -18,29 +12,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
 
 @Api("Dropwizard Client API")
 @Path("/api")
 public class ClientController {
     ClientService clientService = new ClientService();
-    ProjectService projectService = new ProjectService();
-    SalesEmployeeService salesEmployeeService = new SalesEmployeeService();
 
     @GET
     @Path("/clients")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllClients() {
+    public Response getAllClientsInfo() {
         try {
-            List<ClientOutput> clientOutputList = new ArrayList<>();
-            for (Client client: clientService.getAllClients()) {
-                SalesEmployee salesEmployee = salesEmployeeService.getSalesEmployeeById(client.getSalesEmpId());
-                List<Project> projectList = projectService.getAllProjectsWithClient(client.getClientId());
-                ClientOutput clientOutput = new ClientOutput(client, salesEmployee, projectList);
-                clientOutputList.add(clientOutput);
-            }
-            return Response.ok(clientOutputList).build();
+            return Response.ok(clientService.getAllClientsInfo()).build();
         } catch (FailedToGetClientsException | FailedToGetProjectsException |
                  FailedToGetSalesEmployeeException e) {
             System.err.println(e.getMessage());
